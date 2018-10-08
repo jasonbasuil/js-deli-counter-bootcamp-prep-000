892b989a010301ede39cee9b8656d025347bd20b
Jason Basuil <basuilj@gmail.com>
2018-10-08 05:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function takeANumber (katzDeliLine, newPerson){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
-    return (`'Currently serving '+ ${katzDeliLine.shift()} + '.')
+    return (`'Currently serving ${katzDeliLine.shift()}.`)
   }
   else {
     return ('There is nobody waiting to be served!')
