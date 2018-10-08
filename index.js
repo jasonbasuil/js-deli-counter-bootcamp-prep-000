a63fc6dd980368f8870997a78e9576c705ff5d2c
Jason Basuil <basuilj@gmail.com>
2018-10-08 05:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,8 @@
 
 function takeANumber (katzDeliLine, newPerson){
   katzDeliLine.push(`${newPerson}`);
-  var currentLineNumber = katzDeliLine.length;
   
-  return (`'Welcome, ${newPerson}. You are number ${currentLineNumber} in line.'`);
+  return (`'Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.'`);
 }
 
 function nowServing(katzDeliLine){
