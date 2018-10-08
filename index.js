7af2c3301e7e0764e4ba2e094d7df9fa56dd9f79
Jason Basuil <basuilj@gmail.com>
2018-10-08 05:45:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+//we need let customers take a number when they enter our store! Woo! So let's build a great function for them below!
+
+// var katzDeliLine = []; 
+
+function takeANumber (katzDeliLine, newPerson){
+  katzDeliLine.push(newPerson);
+  var currentLineNumber = katzDeliLine.length;
+  
+  return (`'Welcome, ${newPerson}. You are number ${currentLineNumber} in line.'`);
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0) {
+    console.log('Currently serving '+ katzDeliLine.shift() + '.')
+  }
+  else {
+    console.log('There is nobody waiting to be served!')
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length > 0) {
+    for (var i = 0; i < katzDeliLine.length; i++;) {
+      console.log(`The line is currently: ${[i]}. ${katzDeliLine[i]}`)
+    }}
+  else {
+    console.log('The line is currently empty')
+  }
+}
